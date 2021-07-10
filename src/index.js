9e35952298cec3e27d2ef63f08e615afbbc72963
bikal1000 <bikal1000.bs@gmail.com>
2021-07-10 09:19:58 +0545

    env change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV == 'development') require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const express = require('express')
 require('./db/mongoose')
 const userRouters = require('./routers/user')
